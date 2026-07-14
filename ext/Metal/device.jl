7fcf0e1bed6624ccd7bb8f4d38ba748b7d03da6f
PraneethMerugu <merugufam@gmail.com>
2026-07-14 17:02:53 -0400

    Fix Metal API for v1.1

--- ext/Metal/device.jl
+++ ext/Metal/device.jl
@@ -3,14 +3,14 @@ import KernelIntrinsics: select_device!, get_warpsize, devices, name, device, de
 select_device!(::MetalBackend, i::Integer) = (i == 1 || @warn "Metal exposes a single device; ignoring index $i")
 devices(::MetalBackend) = [Metal.device()]
 
-name(dev::Metal.MTL.MTLDeviceInstance) = String(dev.name)
+name(dev::Metal.MTL.MTLDevice) = String(dev.name)
 
 device(src::Metal.MtlArray) = Metal.device(src)
 device(::MetalBackend) = Metal.device()
 
-deviceid(dev::Metal.MTL.MTLDeviceInstance) = 1  # Metal only has one device
+deviceid(dev::Metal.MTL.MTLDevice) = 1  # Metal only has one device
 
-function get_warpsize(dev::Metal.MTL.MTLDeviceInstance = Metal.device())
+function get_warpsize(dev::Metal.MTL.MTLDevice = Metal.device())
     kernel = @metal launch=false (() -> nothing)()  # dummy kernel
     pipeline = kernel.pipeline
     Int(pipeline.threadExecutionWidth)
